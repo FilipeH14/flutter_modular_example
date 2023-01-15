8735bb36bd9a3c5a1bdac578a36168d5e4ea3194
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-01-15 16:03:39 -0300

    iniciando configuração do modular

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_example/app/categoria/categoria_module.dart';
+import 'package:flutter_modular_example/app/splash/splash_page.dart';
 
 class AppModule extends Module {
-  
+  @override
+  List<Bind<Object>> get binds => const [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(Modular.initialRoute,
+            child: (context, args) => const SplashPage()),
+        ModuleRoute('/categoria', module: CategoriaModule()),
+      ];
 }
---
+++ lib/app/categoria/categoria_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoriaPage extends StatelessWidget {
+  const CategoriaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('categoria page')),
+      body: Container(),
+    );
+  }
+}
